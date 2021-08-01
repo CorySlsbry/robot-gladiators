189d5006bf72d2a3920024605f82efcb50e1f0d5
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-08-01 17:52:16 -0500

    robot game starting

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function names "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+// window.alert(playerName);
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message here");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+// fight();
